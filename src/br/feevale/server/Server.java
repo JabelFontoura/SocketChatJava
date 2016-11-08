24e02142a399a936c9fce4139a6f61805fc72f19
JabelFontoura <jabel_fontoura@hotmail.com>
2016-11-08 13:28:30 -0200

    Update Server.java

--- src/br/feevale/server/Server.java
+++ src/br/feevale/server/Server.java
@@ -1,3 +1,6 @@
+/* 
+ Author: Jabel Fontoura
+*/
 package br.feevale.server;
 
 import java.io.BufferedReader;
@@ -118,7 +121,7 @@ public class Server extends Thread {
 			clientes = new ArrayList<BufferedWriter>();
 			JOptionPane.showMessageDialog(null,"Servidor ativo na porta: "+ txtPort.getText());
 			while(true){
-				System.out.println("Aguardando conex„o...");
+				System.out.println("Aguardando conex√£o...");
 				Socket con = server.accept();
 				System.out.println("Cliente conectado...");
 				Thread t = new Server(con); t.start();
